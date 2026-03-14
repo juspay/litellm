f76e95d5f25e75e085564f55b1b8531352ba8af3
Murarry Reddy <murarry.reddy@juspay.in>
2026-03-14 14:37:35 +0530

    performance fixes for rpm and concurrency limit: avoiding addtional writes for increments and decrements on Redis counters

--- litellm/main.py
+++ litellm/main.py
@@ -1539,6 +1539,9 @@ def completion(  # type: ignore # noqa: PLR0915
             litellm_request_debug=kwargs.get("litellm_request_debug", False),
             tpm=kwargs.get("tpm"),
             rpm=kwargs.get("rpm"),
+            is_centralized_redis_cache_incremented=kwargs.get(
+                "is_centralized_redis_cache_incremented"
+            ),
         )
         cast(LiteLLMLoggingObj, logging).update_environment_variables(
             model=model,
